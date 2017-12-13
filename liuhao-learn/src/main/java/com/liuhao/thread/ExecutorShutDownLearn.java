2d1c31a793770ce4b38475c79cb94bbbee2ef318
liuhao <liuhao@sunyuki.com>
2017-12-13 15:10:29 +0800

    包替换 com.{name}.{contentName}

--- liuhao-learn/src/main/java/com/liuhao/thread/ExecutorShutDownLearn.java
+++ liuhao-learn/src/main/java/com/liuhao/thread/ExecutorShutDownLearn.java
@@ -1,4 +1,4 @@
-package com.liuhao;
+package com.liuhao.thread;
 
 public class ExecutorShutDownLearn {
 
@@ -6,4 +6,6 @@ public class ExecutorShutDownLearn {
     public static void main(String[] args) {
         System.out.println("1111");
     }
+
+
 }
